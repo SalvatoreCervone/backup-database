bb3e89e6845a75e1ce80cac01111dae8d472f2f1
Salvatore <salvatore999@virgilio.it>
2025-04-08 19:10:17 +0200

    add list of connections for multi database backup

--- config/backup-database.php
+++ config/backup-database.php
@@ -2,10 +2,12 @@
 
 // config for Salvatore/BackupDatabase
 return [
-    'connection' => env('DB_CONNECTION'),
-    'dbname' => env('DB_DATABASE'),
-    'dbhost' => env('DB_HOST'),
-    'daily' => false,
-    'destinationpath' => 'c:\tmp\\',
+    'listconnection' => [
+        'connection' => env('DB_CONNECTION'),
+        'dbname' => env('DB_DATABASE'),
+        'dbhost' => env('DB_HOST'),
+        'daily' => false,
+        'destinationpath' => 'c:\tmp\\',
+    ],
     // 'base' => "BACKUP DATABASE SQLTestDB TO DISK = 'c:\tmp\SQLTestDB.bak'   WITH FORMAT,    NAME = 'Full Backup of SQLTestDB';"
 ];
--- src/BackupDatabase.php
+++ src/BackupDatabase.php
@@ -14,22 +14,25 @@ class BackupDatabase
     public function backup()
     {
         // "BACKUP DATABASE SQLTestDB TO DISK = 'c:\tmp\SQLTestDB.bak'   WITH FORMAT,      MEDIANAME = 'SQLServerBackups',      NAME = 'Full Backup of SQLTestDB';"
-        $connection = config("backup-database.connection");
-        $driver = config("database.connections.{$connection}.driver");
+        $listconnections = config('backup-database.listconnection');
+        foreach ($listconnections as $connection) {
+            $connection = config("backup-database.connection");
+            $driver = config("database.connections.{$connection}.driver");
 
-        if ($driver == 'sqlsrv') {
-            $dbhost = config('backup-database.dbhost');
-            $dbname = config('backup-database.dbname');
-            $daily = config('backup-database.daily');
-            $destinationpath = config('backup-database.destinationpath');
-            $name = $dbname  .  ($daily ? "_" . Carbon::now()->format('Y-m-d') : "") . ".bak";
-            $user = env('DB_USERNAME');
-            $password = env('DB_PASSWORD');
-            $script = "BACKUP DATABASE " . $dbname . " TO DISK= '" . $destinationpath . $name . "'";
-            $result = shell_exec('sqlcmd -S ' . $dbhost . ' -U ' .  $user . ' -P ' . $password . ' -Q "' . $script . '"');
-            return $result;
+            if ($driver == 'sqlsrv') {
+                $dbhost = config('backup-database.dbhost');
+                $dbname = config('backup-database.dbname');
+                $daily = config('backup-database.daily');
+                $destinationpath = config('backup-database.destinationpath');
+                $name = $dbname  .  ($daily ? "_" . Carbon::now()->format('Y-m-d') : "") . ".bak";
+                $user = env('DB_USERNAME');
+                $password = env('DB_PASSWORD');
+                $script = "BACKUP DATABASE " . $dbname . " TO DISK= '" . $destinationpath . $name . "'";
+                $result = shell_exec('sqlcmd -S ' . $dbhost . ' -U ' .  $user . ' -P ' . $password . ' -Q "' . $script . '"');
+                return $result;
+            }
+            throw new \Exception("Unsupported database driver: {$driver}");
         }
-        throw new \Exception("Unsupported database driver: {$driver}");
     }
 
     public function restore()
